946eade198cabfdd20c9af5fda99b68ec1791048
alyssacazarte <leenunuyaa@gmail.com>
2023-10-08 17:18:07 +0800

    Database Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,12 +17,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\Appointment::factory(10)->create();
-        // \App\Models\Schedule::factory(10)->create();
-        // \App\Models\Service::factory(10)->create();
-        // \App\Models\Timeslot::factory(10)->create();
-
-        User::factory(1)->create();
-        // php artisan migrate:fresh seeder
+        DB::table('users')->insert([
+            'name' => 'Favio Jasso',
+            'email' => 'leenunuyaa@gmail.com',
+            'role' =>  1,
+            'password' => '$2y$10$jQrPXQPbpswOcdi/MhisDuTE/CHhzKEjpOS73Gs/FA0S6IL6oPYly', // password
+        ]);
     }
 }
