a1f931afc0a98d1642c6fde2188ca6dfef4f0fc8
alyssacazarte <leenunuyaa@gmail.com>
2023-10-08 17:00:43 +0800

    Change in UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,7 +26,7 @@ class UserFactory extends Factory
             'name' => 'Favio Jasso',
             'email' => 'leenunuyaa@gmail.com',
             'role' =>  1,
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => '$2y$10$jQrPXQPbpswOcdi/MhisDuTE/CHhzKEjpOS73Gs/FA0S6IL6oPYly', // password
         ];
     }
 }
